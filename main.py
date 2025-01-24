a3796332e7b42316c25ff105bfab6594303ba9cd
valdaskalvis <kalvis.valdas@gmail.com>
2025-01-24 13:25:39 +0200

    Single player game mode against a randomised opponent ("Monkey") added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions.print import spausdinam
 from functions.check import check_X, check_0, check_draw
+from random import randrange
 
 while True:
     choice = int(input("1 - start two player game\n2 - start game against Monkey\n3 - start game vs and AI opponent\n4 - see scores\n0 - exit\n"))
@@ -53,8 +54,50 @@ while True:
                     print("Invalid choice. Try again.")
 
         case 2:
-            player_vs_monkey_name = input("Enter player name. You will be playing with X-s.\n")
+            monkey_game_on = True
+            print("You will be playing with X-s.")
             print("You will be playing against an opponent of a very limited intelligence. Would be a shame to lose here!")
+            sarasas = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            while monkey_game_on:
+                spausdinam(sarasas)
+                try:
+                    # player turn: choose an available field, upon correct choice - update the field and check for end of game scenarios
+                    choice_X = int(input(f"Please choose an available field to place X: "))
+                    if type(sarasas[choice_X - 1]) is int and choice_X != 0:
+                        sarasas[choice_X - 1] = "X"
+                        if check_X(sarasas) is True:
+                            spausdinam(sarasas)
+                            print(f"You won!\nPlay again?\n")
+                            break
+                        if check_draw(sarasas) is True:
+                            spausdinam(sarasas)
+                            print("It's a draw!\nPlay again?\n")
+                            break
+                    else:
+                        print("Invalid choice. Try again.")
+                        continue
+                    spausdinam(sarasas)
+                    print("Monkey smashes the keyboard!")
+                    # monkey turn: choose an available field at random, update the field and check for end of game scenarios
+                    monkey_turn = True
+                    while monkey_turn:
+                        try:
+                            choice_0 = randrange(1, 9)
+                            if type(sarasas[choice_0 - 1]) is int and choice_0 != 0:
+                                print(f"Monkey hits {sarasas[choice_0 - 1]}")
+                                sarasas[choice_0 - 1] = "0"
+                                monkey_turn = False
+                                if check_0(sarasas) is True:
+                                    spausdinam(sarasas)
+                                    print(f"Monkey won - shame on you!\nPlay again?\n")
+                                    monkey_game_on = False
+                            else:
+                                continue
+                        except ValueError:
+                            continue
+                except (IndexError,ValueError):
+                    print("Invalid choice. Try again.")
+
         case 3:
             player_vs_ai_name = input("Enter player name. You will be playing with X-s.\n")
             print("You will be playing against an AI opponent, hopefully it'll be a bit of a challenge!")
